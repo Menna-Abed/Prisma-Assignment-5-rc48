582902f18f2474e183938e8467cd2b5546fdc2b7
Menna Abed <mennaabed2005@gmail.com>
2026-09-05 23:27:20 +0300

    Update comment.route.js

--- Prisma Assignment5/src/app/comment/comment.route.js
+++ Prisma Assignment5/src/app/comment/comment.route.js
@@ -10,14 +10,14 @@ const {
 
 const router = Router();
 
-// 1. Static and Specific Routes
+ 
 router.post('/', createBulkComments);
 router.post('/find-or-create', findOrCreateComment);
 router.get('/search', searchComments);
 router.get('/newest/:postId', getNewestComments);
 router.get('/details/:id', getCommentDetails);
 
-// 2. Dynamic Param Routes
+ 
 router.patch('/:commentId', updateComment);
 
 module.exports = router;
